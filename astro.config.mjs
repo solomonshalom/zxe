bbe70c4d7f1be27e02bf9976f5891010d5869a5a
Solomon Shalom Lijo <solomonlijo@skiff.com>
2024-06-23 15:49:11 +0530

    Hello! Mom

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yoursite.com/",
+  site: "https://zxe.solomonlijo.com",
   integrations: [tailwind(), sitemap()]
 });
